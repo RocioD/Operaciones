f49766f7af9ecdcd49a8a223062a62007879fcb7
Rocío Díaz San Martín <rocio.diaz-sm@hotmail.com>
2019-09-25 15:28:41 -0300

    primer commit

---
+++ OperacionesBasicas/OperacionesBasicas.java
@@ -0,0 +1,31 @@
+public class OperacionesBasicas{
+
+    public int Sumar(int a, int b){
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return a + b;
+    }
+
+    public int Restar(int a, int b){
+        if (a > b) {
+           return a - b; 
+        }
+        return 0;
+    }
+
+    public int Multiplicar(int a, int b){
+        if (b == 0) {
+           return 1; 
+        }
+        return a * b;
+    }
+
+    public int Dividir(int a, int b){
+        if (b == 0) {
+            return 0; 
+         }
+         return a / b;
+    }
+
+}
